9e7dd1c0ea1f00dacd97ea639021d11fc69b5216
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2019-01-08 20:36:13 -0800

    Wiring up delay processor (#32)

--- Source/Processors/DelayProcessor.cpp
+++ Source/Processors/DelayProcessor.cpp
@@ -1,24 +1,17 @@
 #include "DelayProcessor.h"
 
-void DelayProcessor::DelayChannel::setReadPtr()
-{
-    readPtr++;
-    if (readPtr >= length) //wrap
-        readPtr = 0;
-}
-
-void DelayProcessor::DelayChannel::setWritePtr()
+void DelayProcessor::DelayChannel::setWritePtr (int maxLength)
 {
     writePtr++;
-    if (writePtr >= length) //wrap
+    if (writePtr >= maxLength) //wrap
         writePtr = 0;
 }
 
 double DelayProcessor::getTailLengthSeconds() const
 {
     int maxLengthSamples = 0;
-    for (auto ch : dChannels)
-        maxLengthSamples = jmax<int> (ch.length, maxLengthSamples);
+    for (auto& ch : dChannels)
+        maxLengthSamples = jmax ((int) ch.length.getTargetValue(), maxLengthSamples);
 
     return (double) maxLengthSamples / getSampleRate();
 }
@@ -26,22 +19,26 @@ double DelayProcessor::getTailLengthSeconds() const
 void DelayProcessor::setLengthMs (int channel, double lengthMs)
 {
     int newLength = (int) (lengthMs * getSampleRate() / 1000.0);
-    dChannels[channel].length = newLength;
+    newLength = jmin (newLength, bufferSize);
+    dChannels[channel].length.setValue ((float) newLength);
 }
 
 void DelayProcessor::prepareToPlay (double sampleRate, int maximumExpectedSamplesPerBlock)
 {
     setRateAndBufferSizeDetails (sampleRate, maximumExpectedSamplesPerBlock);
-    delayBuffer.setSize (2, (int) sampleRate * 10);
-
+    bufferSize = (int) sampleRate * maxDelaySeconds;
+    delayBuffer.setSize (2, bufferSize);
     delayBuffer.clear();
+
+    for (auto& ch : dChannels)
+        ch.length.reset (sampleRate, 1e-2);
 }
 
 void DelayProcessor::releaseResources()
 {
     delayBuffer.clear();
     
-    for (auto ch : dChannels)
+    for (auto& ch : dChannels)
         ch.resetPtrs();
 }
 
@@ -57,12 +54,24 @@ void DelayProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& /*mid
 
 float DelayProcessor::delay (int channel, float x)
 {
-    float y = delayBuffer.getSample (channel, dChannels[channel].readPtr);	//read from buffer
-    delayBuffer.setSample (channel, dChannels[channel].writePtr, x);			//write to buffer
-    
+    auto& ch = dChannels[channel];
+    float y = x;
+
+    const float len = ch.length.getNextValue();
+    const float fractionSample = len - (int) len;
+    const int readPtr = (int) (ch.writePtr - len + bufferSize) % bufferSize;
+
+    if (readPtr != ch.writePtr)
+    {
+        float samp0 = delayBuffer.getSample (channel, readPtr);
+        float samp1 = delayBuffer.getSample (channel, (readPtr + 1) % bufferSize);
+        y = samp0 + fractionSample * (samp1 - samp0);
+
+        delayBuffer.setSample (channel, ch.writePtr, x);	    //write to buffer
+    }
+
     //update pointers
-    dChannels[channel].setReadPtr();
-    dChannels[channel].setWritePtr();
+    ch.setWritePtr (bufferSize);
 
     return y;
 }
--- Source/Processors/TrackProcessor.cpp
+++ Source/Processors/TrackProcessor.cpp
@@ -2,6 +2,17 @@
 #include "../Tracks/Track.h"
 #include "../MainComponent.h"
 
+namespace
+{
+    constexpr float roomWidth = 3.0f; //meters
+    constexpr float headWidth = 0.215f; //meters
+
+    constexpr float leftEarX = headWidth / 2.0f;
+    constexpr float rightEarX = - leftEarX;
+
+    constexpr float speedOfSound = 343.0f;
+}
+
 TrackProcessor::TrackProcessor (File& file) : ProcessorBase (String ("Track Processor"))
 {
     formatManager.registerBasicFormats();
@@ -54,16 +65,28 @@ void TrackProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiM
     delayProcessor->processBlock (buffer, midiMessages);
     panProcessor->processBlock (buffer, midiMessages);
     distProcessor->processBlock (buffer, midiMessages);
-    reverbProcessor->processBlock (buffer, midiMessages);
+    //reverbProcessor->processBlock (buffer, midiMessages);
 
     if (isMute || soloState == otherTrack)
         buffer.clear();
 }
 
-void TrackProcessor::trackMoved (int x, int y, int width)
+void TrackProcessor::trackMoved (int x, int y, int width, bool mouseUp)
 {
     float gain = powf ((float) width / (float) Track::maxWidth, 5.0f);
     gainProcessor->setGain (gain);
+
+    if (mouseUp) //adjust delay
+    {
+        const float trackX = (float) (x - MainComponent::width / 2) * roomWidth / (float) MainComponent::width;
+        const float trackY = (float) (MainComponent::height - y) * roomWidth / (float) MainComponent::height;
+
+        const float leftDelay  = sqrtf (powf (trackX - leftEarX, 2)  + powf (trackY, 2)) * 1000.0f / speedOfSound;
+        const float rightDelay = sqrtf (powf (trackX - rightEarX, 2) + powf (trackY, 2)) * 1000.0f / speedOfSound;
+
+        delayProcessor->setLengthMs (0, leftDelay);
+        delayProcessor->setLengthMs (1, rightDelay);
+    }
 
     float pan = (float) x / (float) MainComponent::width;
     panProcessor->setPan (pan);
