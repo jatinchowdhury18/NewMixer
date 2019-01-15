d39b4e2b1433308b68f699e5ce64fb982f53d6db
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-01-14 22:09:46 -0800

    Update Jucer file for release

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -42,8 +42,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "NewMixer";
-    const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  companyName    = "Chowdhury DSP";
+    const char* const  versionString  = "0.1.0";
+    const int          versionNumber  = 0x100;
 }
 #endif
