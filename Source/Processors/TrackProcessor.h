0f73e5aed4167f6ab3abf52d0c8c27c03848d4b3
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-05-01 18:56:45 -0700

    Implement multi-rate file reading in track processor

--- Source/Processors/TrackProcessor.h
+++ Source/Processors/TrackProcessor.h
@@ -17,20 +17,26 @@ public:
     TrackProcessor (MemoryInputStream* input, int64 startSample = 0);
     TrackProcessor (const TrackProcessor& processor);
 
+    void prepareToPlay (double sampleRate, int maxExpectedBlockSize) override;
     void processBlock (AudioBuffer<float> &buffer, MidiBuffer &midiMessages) override;
+    void releaseResources() override;
     
-    int64 getLengthSamples() const override { return reader->lengthInSamples; }
+    int64 getLengthSamples() const override { return transportSource.getTotalLength(); }
     AudioFormatReader* getReader() const { return reader; }
     AudioFormatManager& getFormatManager() { return formatManager; }
 
     File getFile() const { return file; }
     void setFile (File& newFile);
 
+    void setupTransportSource (InputStream* inputStream);
+
 private:
     File file;
 
     AudioFormatManager formatManager;
     AudioFormatReader* reader;
+    std::unique_ptr<AudioFormatReaderSource> readerSource;
+    AudioTransportSource transportSource;
 
     JUCE_LEAK_DETECTOR (TrackProcessor)
 };
