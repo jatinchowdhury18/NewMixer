61a4bff67573aef4d9e251fcf5ebd2829fcb3781
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2019-09-23 09:31:41 -0700

    Bypass and preset controls for plugins (#92)

--- Source/GUI Extras/PluginWindow.cpp
+++ Source/GUI Extras/PluginWindow.cpp
@@ -5,13 +5,18 @@ enum
 {
     pluginX = 400,
     pluginY = 300,
+
+    minTopBarWidth = 300,
+    topBarHeight = 50,
+    bypassHeight = 25,
+    bypassWidth = 75,
 };
 
 PluginWindow::PluginWindow (AudioPluginInstance* plugin, Track* track) :
     DocumentWindow (track->getName(), Colours::lightgrey, DocumentWindow::closeButton),
-    editor (plugin->createEditor())
+    comp (plugin)
 {
-    WindowHelper::setupDefaultDocumentWindow (*this, editor);
+    WindowHelper::setupDefaultDocumentWindow (*this, &comp);
     setTopLeftPosition (pluginX, pluginY);
     setResizable (false, false);
     setVisible (true);
@@ -23,4 +28,75 @@ PluginWindow::PluginWindow (AudioPluginInstance* plugin, Track* track) :
 void PluginWindow::closeButtonPressed()
 {
     setVisible (false);
+}
+
+//=======================================================================
+PluginContainerComponent::PluginContainerComponent (AudioPluginInstance* plugin) :
+    editor (plugin->createEditor())
+{
+    addAndMakeVisible (editor.get());
+    resized();
+
+    auto setupButton = [this] (Button& button, String text = {}, std::function<void()> onClick = {})
+    {
+        button.setButtonText (text);
+        button.setColour (TextButton::buttonColourId, Colours::transparentBlack);
+        button.setColour (TextButton::buttonOnColourId, Colours::antiquewhite);
+        button.setColour (TextButton::textColourOffId, Colours::antiquewhite);
+        button.setColour (TextButton::textColourOnId, Colours::black);
+        button.setColour (ComboBox::outlineColourId, Colours::antiquewhite);
+        button.setClickingTogglesState (true);
+        button.onClick = onClick;
+        addAndMakeVisible (button);
+    };
+
+    setupButton (bypassButton, "BYPASS",
+                [this, plugin] { plugin->getBypassParameter()->setValueNotifyingHost (bypassButton.getToggleState()); });
+    setupProgramsBox (plugin);
+}
+
+PluginContainerComponent::~PluginContainerComponent()
+{  
+}
+
+void PluginContainerComponent::setupProgramsBox (AudioPluginInstance* plugin)
+{
+    programsBox.setColour (ComboBox::backgroundColourId, Colours::transparentBlack);
+    programsBox.setColour (ComboBox::outlineColourId, Colours::antiquewhite);
+    programsBox.setColour (ComboBox::textColourId, Colours::antiquewhite);
+    programsBox.setColour (ComboBox::arrowColourId, Colours::antiquewhite);
+    addAndMakeVisible (programsBox);
+
+    for (int i = 0; i < plugin->getNumPrograms(); ++i)
+        programsBox.addItem (plugin->getProgramName (i), i+1);
+
+    programsBox.setSelectedItemIndex (plugin->getCurrentProgram(), dontSendNotification);
+    programsBox.onChange = [this, plugin] { plugin->setCurrentProgram (programsBox.getSelectedItemIndex()); };
+}
+
+void PluginContainerComponent::childBoundsChanged (Component* child)
+{
+    if (child == editor.get())
+        resized();
+}
+
+void PluginContainerComponent::resized()
+{
+    auto pluginWidth = editor->getWidth();
+    auto pluginHeight = editor->getHeight();
+
+    setSize (jmax<int> (pluginWidth, minTopBarWidth), pluginHeight + topBarHeight);
+    editor->setBounds (pluginWidth > minTopBarWidth ?  0 :  (minTopBarWidth - pluginWidth) / 2, topBarHeight,
+                       pluginWidth, pluginHeight);
+
+    bypassButton.setBounds (5, (topBarHeight - bypassHeight) / 2, bypassWidth, bypassHeight);
+    programsBox.setBounds (bypassButton.getRight() + 10, bypassButton.getY(), 2*bypassWidth, bypassHeight);
+}
+
+void PluginContainerComponent::paint (Graphics& g)
+{
+    g.fillAll (Colours::grey);
+
+    g.setColour (Colours::darkgrey.darker());
+    g.fillRect (0, 0, getWidth(), topBarHeight);
 }
